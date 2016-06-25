6ecf2219b14298594f05e287d6c3f3c2ec06fa19
ryanluker <ryanluker@outlook.com>
2016-06-25 11:36:58 -0700

    use ms and setup auto function with looping

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var Gpio        = require('onoff').Gpio;
 var powerTail   = new Gpio(408, 'out');
 
 //misc variables
-var INCREMENT   = 900; //15min
+var INCREMENT   = 900000; //15min
 var TEMPERATURE = 24; //24C
 var WEATHERAPI  = 'http://api.openweathermap.org/data/2.5/weather?q=Kelowna,ca&appid=794fc23dd8ce7ba66cd0361f58355010&units=metric';
 
@@ -49,20 +49,43 @@ program
     INCREMENT = options.inc || INCREMENT;
     TEMPERATURE = options.temp || TEMPERATURE;
     console.log('State: ' + STATE + ' INC: ' + INCREMENT + ' Temp: ' + TEMPERATURE);
-    request(WEATHERAPI, function(err, res, body) {
-      if(err) //run shutdown
-      console.log('Current Temp: ' + JSON.parse(body).main.temp);
-      if(TEMPERATURE < JSON.parse(body).main.temp) {
-        //switch on
-      } else {
-        //switch off
-      }
-    });
+    auto(INCREMENT, TEMPERATURE);
   });
+
+/**
+ * auto function that begins an infinite loop around whether to turn on or off
+ * @param {integer} inc - wait time in seconds between loops
+ * @param {integer} temp - temperature in C for the activation point 
+ */
+function auto(inc, temp) {
+  request(WEATHERAPI, function(err, res, body) {
+    //continue auto operation even if http request fails
+    if(err) {
+      console.log(new Date() + ': Error');
+    } else {
+      //decide whether to turn on or off
+      console.log(new Date() + ': Current Temp (' + JSON.parse(body).main.temp + ')');
+      if(temp < JSON.parse(body).main.temp) {
+        console.log(new Date() + ': Switching on');
+        powerTail.writeSync(0);
+      } else {
+        console.log(new Date() + ': Switching off');
+        powerTail.writeSync(1);
+      } 
+    }
+
+    //set timeout for next check in
+    setTimeout(function(){
+      auto(inc, temp);
+    }, inc);
+  });
+};
 
 program.parse(process.argv);
 
 //remove resources
 process.on('SIGINT', function () {
+  console.log(new Date() + ': Shutting down!');
   powerTail.unexport();
+  process.exit(0);
 });
